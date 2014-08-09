f847be1b9a349224e4fa5eff6f02f003059de983
Max Ehrlich <max.ehr@gmail.com>
2014-08-09 12:37:06 -0500

    Added page for adding food with a specific amount and metric

---
+++ web/addFood.php
@@ -0,0 +1,56 @@
+<?php
+
+include('utils/checkUser.php');
+include('utils/model.php');
+
+$user = getUser($seshUser);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['meal_id']))
+{
+    $meal = getMeal($_POST['meal_id']);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['food_to_add']))
+{
+    $newFood = $_POST['food_to_add'];
+
+    if(!isset($meal))
+    {
+        $meal = createMeal($user['email']);
+    }
+
+    $food = getFood($newFood);
+    $metrics = getFoodMetrics($food['ndb_no']);
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Amount of Food</title>
+    </head>
+    <body>
+        <h1>Add <?= $food['name'] ?> to Meal</h1>
+        <form action="addMeal.php" method="POST">
+            <label for="amount">Amount</label>
+            <input type="number" name="amount" step="any" min="0"/>
+            <select name="metric">
+                <?php
+
+                foreach($metrics as $m)
+                {
+                    ?>
+
+                    <option value="<?=$m?>"><?=$m?></option>
+
+                    <?php
+                }
+
+                ?>
+            </select>
+            <button type="submit">Add</button>
+            <input type="hidden" name="meal_id" value="<?= $meal['meal_id'] ?>"/>
+            <input type="hidden" name="food_to_add" value="<?= $food['ndb_no'] ?>"/>
+        </form>
+    </body>
+</html>
