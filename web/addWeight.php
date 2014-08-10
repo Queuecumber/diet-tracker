60c5dc4c2cf844c3cf5d4b257352407255a54598
Max Ehrlich <max.ehr@gmail.com>
2014-08-09 20:58:21 -0500

    Added bootstrap styling to the weight page

--- web/addWeight.php
+++ web/addWeight.php
@@ -16,44 +16,112 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['new_weight']))
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Weight</title>
+
+        <link rel="stylesheet" href="lib/bootstrap-3.2.0-dist/css/bootstrap.min.css"/>
+        <script src="lib/jquery-2.1.1.min.js"></script>
+        <script src="lib/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+
+        <style>
+
+        main
+        {
+            padding: 10px;
+        }
+
+        .previous-weights
+        {
+            width: 300px;
+            margin-top: 10px;
+        }
+
+        .weight-record
+        {
+            margin-top: 5px;
+        }
+
+        </style>
+
     </head>
     <body>
         <header>
-            <h1>Record a new Weight</h1>
-            <form action="addWeight.php" method="POST">
-                <label for="new_weight">Weight</label>
-                <input name="new_weight" type="number" required/>lbs
-                <button type="submit">Record</button>
-            </form>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#dt-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand">Diet Tracker</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="dt-navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php"><span class="glyphicon glyphicon-list"></span> Daily Report</a></li>
+                            <li><a href="addMeal.php"><span class="glyphicon glyphicon-cutlery"></span> Add Meal</a></li>
+                            <li class="active"><a href="addWeight.php"><span class="glyphicon glyphicon-inbox"></span> Record Weight</a></li>
+                            <li><a href="addTarget.php"><span class="glyphicon glyphicon-screenshot"></span> Update Calorie Target</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-time"></span> History</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?=$user['name']?><span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Account Settings</a></li>
+                                    <li><a href="logOff.php"><span class="glyphicon glyphicon-off"></span> Log Off</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
         <main>
-            <?php
-
-            if(count($weights) > 0)
-            {
-                ?>
-                <h2>Previous Weights</h2>
-
+            <section>
+                <h1>Record A New Weight</h1>
+                <form action="addWeight.php" method="POST" class="form-inline">
+                    <div class="input-group">
+                        <input name="new_weight" type="number" class="form-control" placeholder="Weight (lbs)" min="0" step="any" required/>
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-default">Record</button>
+                        </span>
+                    </div>
+                </form>
+            </section>
+            <section class="previous-weights">
                 <?php
 
-                foreach($weights as $w)
+                if(count($weights) > 0)
                 {
                     ?>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Previous Weights</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?php
 
-                    <strong><?= $w['amount'] ?>lbs</strong> at <?= $w['date'] ?> <br/>
+                            foreach($weights as $w)
+                            {
+                                ?>
+
+                                <div class="weight-record">
+                                    <strong><?= $w['amount'] ?>lbs</strong> on <?= date("l, F jS Y" ,strtotime($w['date'])) ?>
+                                </div>
+
+                                <?php
+                            }
+
+                            ?>
+                        </div>
+                    </div>
 
                     <?php
                 }
-
                 ?>
-
-                <?php
-            }
-            ?>
+            </section>
         </main>
-        <footer>
-            <a href="index.php">Done</a>
-        </footer>
     </body>
 </html>
