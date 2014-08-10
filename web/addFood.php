c1678a50552e167848c81777ae7930824bddc14d
Max Ehrlich <max.ehr@gmail.com>
2014-08-09 22:07:09 -0500

    Updated to bootstrap style

--- web/addFood.php
+++ web/addFood.php
@@ -21,14 +21,29 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['food_to_add']))
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Amount of Food</title>
+
+        <link rel="stylesheet" href="lib/bootstrap-3.2.0-dist/css/bootstrap.min.css"/>
+        <script src="lib/jquery-2.1.1.min.js"></script>
+        <script src="lib/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+
+        <style>
+
+        main
+        {
+            padding: 10px;
+        }
+
+        </style>
+
     </head>
     <body>
         <h1>Add <?= $food['name'] ?> to Meal</h1>
-        <form action="addMeal.php" method="POST">
-            <label for="amount">Amount</label>
-            <input type="number" name="amount" step="any" min="0" required/>
-            <select name="metric">
+        <form action="addMeal.php" method="POST" class="form-inline">
+            <input type="number" name="amount" step="any" min="0" placeholder="Number of Servings" class="form-control" required/>
+            <select name="metric" class="form-control">
                 <?php
 
                 foreach($metrics as $m)
