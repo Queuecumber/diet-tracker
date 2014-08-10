d1180737f1d07f94ae959d8b03393951b518568a
Max Ehrlich <max.ehr@gmail.com>
2014-08-10 00:39:22 -0500

    Removed float conversion

--- web/index.php
+++ web/index.php
@@ -112,7 +112,7 @@ foreach($meals as $m)
                                             {
                                                 ?>
 
-                                                <tr class="<?= floatval($i['calories']) > 400 ? "danger" : "" ?>">
+                                                <tr class="<?= $i['calories'] > 400 ? "danger" : "" ?>">
                                                     <td><?= $i['name'] ?></td>
                                                     <td><?= $i['value'] ?></td>
                                                     <td><?= $i['metric'] ?></td>
