2d224e3213d64f5c6d19c2a0c6b5ba86db4ac676
Max Ehrlich <max.ehr@gmail.com>
2014-08-09 20:38:10 -0500

    Restyled login page with bootstrap

--- web/login.php
+++ web/login.php
@@ -58,20 +58,91 @@ else
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Diet Tracker Log In</title>
+
+        <link rel="stylesheet" href="lib/bootstrap-3.2.0-dist/css/bootstrap.min.css"/>
+        <script src="lib/jquery-2.1.1.min.js"></script>
+        <script src="lib/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+
+        <style>
+
+        .form-signin
+        {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+
+        .form-signin .form-control
+        {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+               -moz-box-sizing: border-box;
+                    box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+        }
+
+        .form-signin .form-control:focus
+        {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"]
+        {
+            margin-top:30px;
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"]
+        {
+            margin-bottom: 30px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
+        .container
+        {
+            margin-top: 200px;
+            max-width: 400px;
+            text-align: center;
+        }
+
+        .container .alert
+        {
+            margin-top: 25px;
+        }
+
+        </style>
+
     </head>
     <body>
-        <form action="login.php" method="POST">
-            <label for="user">Email</label>
-            <input type="text" name="user" required/>
-            <br/>
-            <label for="pw">Password</label>
-            <input type="password" name="pw" required>
-            <br/>
-            <button type="submit">Log In</button>
-            <br/>
-            <output name="response" for="user pw"><?= $errorMessage ?></output>
-        </form>
-        <a href="register.php">Register</a>
+        <section class="container">
+
+            <h1>Diet Tracker Log In</h1>
+
+            <form action="login.php" method="POST" class="form-signin">
+                <input type="email" name="user" class="form-control" placeholder="Email" required/>
+                <input type="password" name="pw" class="form-control" placeholder="Password" required>
+                <button type="submit" class="btn btn-lg btn-default btn-block">Log In</button>
+            </form>
+            <a href="register.php">Register</a> <br/>
+            <?php
+
+            if($errorMessage != '')
+            {
+                ?>
+
+                <div class="alert alert-danger"><strong>Oops!</strong> <?= $errorMessage ?></div>
+
+                <?php
+            }
+            ?>
+        </section>
     </body>
 </html>
