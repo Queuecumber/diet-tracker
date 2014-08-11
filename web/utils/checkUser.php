99abeb98eb546db389decb659527788a3de27ba8
Max Ehrlich <max.ehr@gmail.com>
2014-08-10 19:02:40 -0500

    another dst correction

--- web/utils/checkUser.php
+++ web/utils/checkUser.php
@@ -2,14 +2,14 @@
 
 session_start();
 
-function tz_offset_to_name($offset, $dst)
+function tz_offset_to_name($offset)
 {
     $abbrarray = timezone_abbreviations_list();
     foreach ($abbrarray as $abbr)
     {
         foreach ($abbr as $city)
         {
-            if ($city['offset'] == $offset && $city['dst'] == $dst)
+            if ($city['offset'] == $offset)
             {
                 return $city['timezone_id'];
             }
@@ -29,7 +29,10 @@ else
     $timezone = $_SESSION['zone'];
     $dst = filter_var($_SESSION['dst'], FILTER_VALIDATE_BOOLEAN);
 
-    $tzName = tz_offset_to_name($timezone, $dst);
+    if(dst)
+        $timezone -= 3600;
+
+    $tzName = tz_offset_to_name($timezone);
     date_default_timezone_set($tzName);
 }
 
